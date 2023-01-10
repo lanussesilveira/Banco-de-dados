16e845d8eb6147411d75ec0fec76d22a0bf188a8
Lanusse Silveira <lanusse.interiores@gmail.com>
2023-01-10 00:59:24 -0300

    Atualizar README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Banco-de-dados
 
 Projeto da disciplina de Banco de Dados.
+
+## Exemplo de SELECT
+
+```sql
+-- SQL COMMAND
+SELECT * FROM usuario AS u JOIN livro AS l
+ON u.id_livro = l.id_livro;
+-- DESCRIÇÂO
+-- ESTE SELECT FAZ UM JOIN...
+```
---
+++ exemplo-postgres.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS livro (
+  id_livro INT NOT NULL UNIQUE,
+  cod_livro INT NOT NULL UNIQUE,
+  titulo VARCHAR(50) NOT NULL,
+  autor VARCHAR(50) NOT NULL,
+  categoria VARCHAR(10),
+  qtd_paginas INT,
+  localizacao VARCHAR(10),
+  PRIMARY KEY (id_livro)
+);
+
+INSERT INTO livro (id_livro, cod_livro, titulo, autor, categoria, qtd_paginas, localizacao) VALUES
+  ('00001', '00100', 'O pequeno Principe', 'Antoine de Saint-exupery', 'infantil', '96', 'E01P05'),
+  ('00002', '00101', 'A ilha das árvores perdidas', 'Elif Shafak', 'Ficção', '352', 'E04P02'),
+  ('00003', '00102', 'Dom Quixote', 'Miguel de Cervantes Saavedra', 'Ficção', '304', 'E04P05');
+
+CREATE TABLE IF NOT EXISTS usuario (
+  id_usuario INT NOT NULL,
+  matricula INT NOT NULL,
+  cpf INT NOT NULL,
+  nome VARCHAR(50) NOT NULL,
+  endereco VARCHAR(50),
+  telefone INT,
+  id_livro INT,
+  PRIMARY KEY (id_usuario,cpf),
+  FOREIGN KEY (id_livro) REFERENCES livro(id_livro)
+);
+
+INSERT INTO usuario (id_usuario, matricula, cpf, nome, endereco, telefone, id_livro) VALUES
+  ('12345', '2023201', '0230876578', 'Pedro Gustavo', 'Rua das Esmeraldas, 12', '988759900', '0001'),
+  ('12346', '2023202', '0230876579', 'Julia Silva', 'Rua Carlos Chagas, 75', '988750087', '0003'),
+  ('12347', '2023203', '0230876579', 'Clarissa Pereira', 'Rua Dalila Silva, 80', '988751514', '0002');
